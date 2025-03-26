df6bf9d07d06c01228422484e40f4dcda436baa2
Thomas Cram <tcram@ucar.edu>
2025-03-26 14:39:52 -0600

    added auth and transfer clients

--- src/rda_python_globus/lib/__init__.py
+++ src/rda_python_globus/lib/__init__.py
@@ -1,14 +1,11 @@
 import json
 import os
 import logging
-from glob import glob
-from io import StringIO
 import re
 
 import click
 
-from .auth import auth_client, internal_auth_client
-from .search import search_client
+from .auth import token_storage_adapter, auth_client, transfer_client
 
 RDA_BASE_PATH = '/glade/campaign/collections/rda'
 LOGPATH = os.path.join(RDA_BASE_PATH, 'work/tcram/logs/globus')
@@ -32,7 +29,7 @@ def prettyprint_json(obj, fp=None):
 
 def configure_log():
    """ Congigure logging """
-   logfile = os.path.join(LOGPATH, 'dataset-search.log')
+   logfile = os.path.join(LOGPATH, 'dsglobus-app.log')
    loglevel = 'INFO'
    format = '%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(message)s'
    logging.basicConfig(filename=logfile, level=loglevel, format=format)
@@ -44,5 +41,7 @@ __all__ = (
     "validate_dsid",
     "prettyprint_json",
     "configure_log",
-    "config_storage_adapter",
+    "token_storage_adapter",
+    "auth_client",
+    "transfer_client",
 )
---
+++ src/rda_python_globus/lib/auth.py
@@ -0,0 +1,41 @@
+import globus_sdk
+from globus_sdk.tokenstorage import SimpleJSONFileAdapter
+
+AUTH_RESOURCE_SERVER = "auth.globus.org"
+AUTH_SCOPES = ["openid", "profile"]
+TRANSFER_RESOURCE_SERVER = "transfer.api.globus.org"
+TRANSFFER_SCOPES = "urn:globus:auth:scope:transfer.api.globus.org:all"
+
+CLIENT_CONFIG = '/glade/u/home/rdadata/lib/python/globus_rda_quasar_tokens.json'
+
+RDA_QUASAR_CLIENT_ID = "05c2f58b-c667-4fc4-94fb-546e1cd8f41f"
+
+def token_storage_adapter():
+    if not hasattr(token_storage_adapter, "_instance"):
+        token_storage_adapter._instance = SimpleJSONFileAdapter(CLIENT_CONFIG, namespace="DEFAULT")
+    return token_storage_adapter._instance
+
+def internal_auth_client():
+    return globus_sdk.NativeAppAuthClient(RDA_QUASAR_CLIENT_ID, app_name="dsglobus")
+
+def auth_client():
+    authorizer = globus_sdk.ClientCredentialsAuthorizer(internal_auth_client(), AUTH_SCOPES)
+    return globus_sdk.AuthClient(authorizer=authorizer, app_name="dsglobus")
+
+def transfer_client():
+    storage_adapter = token_storage_adapter()
+    as_dict = storage_adapter.read_as_dict()
+
+    authdata = as_dict[TRANSFER_RESOURCE_SERVER]
+    access_token = authdata["access_token"]
+    refresh_token = authdata["refresh_token"]
+    access_token_expires = authdata["expires_at_seconds"]
+    authorizer = globus_sdk.RefreshTokenAuthorizer(
+        refresh_token,
+        internal_auth_client(),
+        access_token,
+        int(access_token_expires),
+        on_refresh=storage_adapter.on_refresh,
+    )
+
+    return globus_sdk.TransferClient(authorizer=authorizer, app_name="dsglobus")
